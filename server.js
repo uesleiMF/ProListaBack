3a7a5705ef6578821b97ceef14941ad9e6989f34
Jose Ueslei De Moraes Ferreira <joseueslei27@gmail.com>
2023-01-26 22:39:21 -0300

    Update server.js
    Server api

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000",https://pro-lista-uesleimf.vercel.app/],
     credentials: true,
   })
 );

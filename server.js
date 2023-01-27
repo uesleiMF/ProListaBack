19aed3760279be3c6009a1d1e8eae302332d0dde
Ueslei <joseueslei2@gmail.com>
2023-01-27 09:48:08 -0300

    2

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000", "https://prolista.vercel.app"],
     credentials: true,
   })
 );
